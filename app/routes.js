6d72e1d3385e1195e8cc5cfd1ef1b346d8ffca62
“Natasha <natasha.dodds1@engineering.digital.dwp.gov.uk>
2025-10-27 16:40:25 +0000

    updated header and started tasks
    GitOrigin-RevId: 9901410085f1191591e28433c355d642ba92bc9b

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,35 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//Persona selection
+
+router.post('/persona-answer', function (req, res) {
+    var persona = req.session.data['persona'];
+    if (persona == "compensator") {
+        res.redirect('/compensator-ui-index');
+    } else if (persona == "agent") {
+        res.redirect('/agent-ui-index');
+    }
+});
+
+
+// Tasks
+router.post('/agent/task-answer', function (req, res) {
+    var task = req.session.data['task'];
+
+    if (task == "CRU 1") {
+        res.redirect('/agent/cru1-tasks');
+    } 
+    else if (task == "Legacy") {
+        res.redirect('/agent/legacy-tasks');
+    } 
+    else if (task == "HS Treatment") {
+        res.redirect('/agent/ni-hs-tasks');
+    } 
+    else {
+        res.redirect('/agent/task-list'); // fallback route
+    }
+});
+
+
