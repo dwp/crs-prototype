c8f026a243056204988430460de2c46e77a267b9
“Olivia <Olivia.Ladanyi1@engineering.digital.dwp.gov.uk>
2025-11-11 12:53:32 +0000

    ni call task
    GitOrigin-RevId: e3717fa08f0a1ab303a9dd64a2170cfd510c77ba

--- app/routes.js
+++ app/routes.js
@@ -63,10 +63,10 @@ router.post('/agent/claim-options-answer', function (req, res) {
         res.redirect('/agent/update-claim-details');
 
     } else if (claimOptions == "send-a-letter") {
-        res.redirect('/agent/confrimation-no-hs');
+        res.redirect('/agent/confrimation-hs-unknown');
         
     } else if (claimOptions == "confirm-hospital-treatment-not-needed") {
-        res.redirect('/agent/confrimation-hs-unknown');
+        res.redirect('/agent/confrimation-no-hs');
     }
 
 });
